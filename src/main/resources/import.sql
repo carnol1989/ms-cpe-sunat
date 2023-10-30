48e26380296ee6c531b9182e3509df5f096e3a89
carnol1989 <carnol1989@hotmail.com>
2023-10-30 11:42:19 -0500

    feat: convert file import utf8

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,8 +1,8 @@
 INSERT INTO tipo_comprobante_pago(codigo, descripcion, abreviatura, ind_estado) VALUES ('01', 'FACTURA', 'F-E', 'S');
 INSERT INTO tipo_comprobante_pago(codigo, descripcion, abreviatura, ind_estado) VALUES ('03', 'BOLETA DE VENTA', 'B', 'S');
-INSERT INTO tipo_comprobante_pago(codigo, descripcion, abreviatura, ind_estado) VALUES ('07', 'NOTA DE CRÉDITO', 'FC', 'S');
-INSERT INTO tipo_comprobante_pago(codigo, descripcion, abreviatura, ind_estado) VALUES ('08', 'NOTA DE DÉBITO', 'FD', 'S');
+INSERT INTO tipo_comprobante_pago(codigo, descripcion, abreviatura, ind_estado) VALUES ('07', 'NOTA DE CRÃ‰DITO', 'FC', 'S');
+INSERT INTO tipo_comprobante_pago(codigo, descripcion, abreviatura, ind_estado) VALUES ('08', 'NOTA DE DÃ‰BITO', 'FD', 'S');
 INSERT INTO tipo_comprobante_pago(codigo, descripcion, ind_estado) VALUES ('R1', 'RECIBO POR HONORARIOS', 'S');
-INSERT INTO tipo_comprobante_pago(codigo, descripcion, ind_estado) VALUES ('R7', 'NOTA CRÉDITO RECIBO POR HONORARIOS', 'S');
-INSERT INTO tipo_comprobante_pago(codigo, descripcion, ind_estado) VALUES ('04', 'LIQUIDACIÓN DE COMPRA', 'S');
-INSERT INTO tipo_comprobante_pago(codigo, descripcion, ind_estado) VALUES ('23', 'PÓLIZA DE ADJUDICACIÓN ELECTRÓNICA', 'S');
+INSERT INTO tipo_comprobante_pago(codigo, descripcion, ind_estado) VALUES ('R7', 'NOTA CRÃ‰DITO RECIBO POR HONORARIOS', 'S');
+INSERT INTO tipo_comprobante_pago(codigo, descripcion, ind_estado) VALUES ('04', 'LIQUIDACIÃ“N DE COMPRA', 'S');
+INSERT INTO tipo_comprobante_pago(codigo, descripcion, ind_estado) VALUES ('23', 'PÃ“LIZA DE ADJUDICACIÃ“N ELECTRÃ“NICA', 'S');
